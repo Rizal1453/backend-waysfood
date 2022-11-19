0c11c9c001d785fd198a53f7e1b7a91338d0c721
Rizal Mantopani <mntpnijal@gmail.com>
2022-11-19 12:32:44 +0700

    update

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -18,7 +18,7 @@ func DatabaseInit() {
 	var DB_NAME = os.Getenv("DB_NAME")
 	var DB_PORT = os.Getenv("DB_PORT")
 	var err error
-	// dsn := "root:@tcp(localhost:3306)/aziz?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:@tcp(localhost:3306)/waysfood?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
